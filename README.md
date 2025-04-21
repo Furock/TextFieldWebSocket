a7c5a22cf0d8ad13e090044456969eab30c88f91
Furock <you@example.com>
2025-04-21 13:20:39 +0200

    Add first working version

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # TextFieldWebSocket
-A websocket experiment to edit text from multiple clients in real-time
+A websocket experiment to edit text from multiple clients in the same network in real-time
+An asgi server implemented with FastAPI (and the startServer script relies on uvicorn)
+
+DON'T use it globally, just in a private local network where you trust every device and person
---
+++ server.py
@@ -0,0 +1,23 @@
+from fastapi import FastAPI, WebSocket, WebSocketDisconnect
+from fastapi.responses import HTMLResponse
+
+app = FastAPI()
+clients = []
+
+@app.get("/")
+async def get():
+    with open("index.html", "r", encoding="utf-8") as f:
+        return HTMLResponse(f.read())
+
+@app.websocket("/ws")
+async def websocket_endpoint(websocket: WebSocket):
+    await websocket.accept()
+    clients.append(websocket)
+    try:
+        while True:
+            data = await websocket.receive_text()
+            for client in clients:
+                if client != websocket:
+                    await client.send_text(data)
+    except WebSocketDisconnect:
+        clients.remove(websocket)
